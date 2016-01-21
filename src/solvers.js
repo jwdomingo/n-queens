695ca212decf71d28b1fb5c73654b2e361f83b20
jwdomingo <johnwill.domingo@gmail.com>
2016-01-21 11:00:57 -0800

    Revert to previous working commit

--- src/solvers.js
+++ src/solvers.js
@@ -15,45 +15,33 @@
 
 
 
-window.findNRooksSolution = function(n, template, start) {
-  template = template || new Board({n: n});
-  start = start || 0;
+window.findNRooksSolution = function(n) {
+  var solution = new Board({n: n});
+    var blank = _.range(n).map(function(){return 0;});
 
-  var blank = _.range(n).map(function(){return 0;});
-  
-  //var topLevel = blank.slice();
+    solution.set(0,[1,0,0,0]);
 
-  window.rookSolutions = [];
-  
-  //topLevel[start] = 1;
-  //template.set(0, topLevel);
+    for (var j = 1; j < solution.rows().length; j++) {
+      
+      var row = solution.get(j);
 
-  for (var row = start; row < template.rows().length; row++) {
-    var currentRow = template.get(row);
+      for (var i = 0; i < row.length; i++) {
+        var arr = blank.slice();
 
-    for (var col = 0; col < currentRow.length; col++) {
-      var arr = blank.slice();
+        arr[i] = 1;
 
-      arr[col] = 1;
+        solution.set(j, arr);
 
-      template.set(row, arr);
-
-      if (!template.hasAnyRooksConflicts()) {
-        debugger;
-        if (start < n - 1) {
-          window.findNRooksSolution(n, template, start + 1);
-        } else {
-          window.rookSolutions.push(template);
+        if (!solution.hasAnyRooksConflicts()) {
+          break;
         }
+
+        solution.set(j, blank);
       }
-
-      template.set(row, blank);
     }
-  }
 
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(window.rookSolutions[0].rows()));
-  return window.rookSolutions[0];
+    console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution.rows()));
+    return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
